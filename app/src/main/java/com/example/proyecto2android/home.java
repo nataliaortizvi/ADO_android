58e012a71e801318af9452ded4c6e1fcac24cdee
Laura <isabelaleon8@gmail.com>
2020-12-09 12:53:20 -0500

    awebo

--- app/src/main/java/com/example/proyecto2android/home.java
+++ app/src/main/java/com/example/proyecto2android/home.java
@@ -3,12 +3,24 @@ package com.example.proyecto2android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class home extends AppCompatActivity {
+
+    private ImageView btnFAmor;
+    private ImageView btnFsisben;
+    private ImageView btnFmundo;
+    private ImageView btnFvet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnFAmor  =  findViewById(R.id.btnFAmor);
+        btnFsisben  =  findViewById(R.id.btnFsisben);
+        btnFmundo  =  findViewById(R.id.btnFmundo);
+        btnFmundo  =  findViewById(R.id.btnFmundo);
+
     }
 }
