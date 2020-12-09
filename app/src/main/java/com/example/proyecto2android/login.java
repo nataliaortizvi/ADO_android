4e8c48a1676501dab886661a127b8087765058e4
nataliaortizvi <natiorvi00@gmail.com>
2020-12-09 12:19:18 -0500

    autenticacion

--- app/src/main/java/com/example/proyecto2android/login.java
+++ app/src/main/java/com/example/proyecto2android/login.java
@@ -3,17 +3,19 @@ package com.example.proyecto2android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class login extends AppCompatActivity implements View.OnClickListener {
 
-    Button logbt, registrobt;
-    EditText mailtx, passtx;
+    private Button notengo, ingresarbt;
+    private EditText mailtx, passtx;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,27 +23,40 @@ public class login extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_login);
 
         //referenciar
-        logbt = findViewById(R.id.logbt);
-        registrobt = findViewById(R.id.registrobt);
+        notengo = findViewById(R.id.ingresarbt);
+        ingresarbt = findViewById(R.id.notengo);
         mailtx = findViewById(R.id.mailtx);
         passtx = findViewById(R.id.passtx);
 
+        auth = FirebaseAuth.getInstance();
+
         //click
-        logbt.setOnClickListener(this);
-        registrobt.setOnClickListener(this);
+        notengo.setOnClickListener(this);
+        ingresarbt.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.logbt:
-                Intent i = new Intent(this, home.class);
-                startActivity(i);
-                finish();
+            case R.id.ingresarbt:
+                auth.signInWithEmailAndPassword(mailtx.getText().toString(), passtx.getText().toString())
+                        .addOnCompleteListener(
+                          task -> {
+                              if(task.isSuccessful()){
+                                  Intent i = new Intent(this, home.class);
+                                  startActivity(i);
+                                  finish();
+                              }else{
+                                  Toast.makeText(this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                              }
+                          }
+                        );
+
+
 
                 break;
-            case R.id.registrobt:
-                Intent e = new Intent(this, signup.class);
+            case R.id.notengo:
+                Intent e = new Intent(this, signn.class);
                 startActivity(e);
                 finish();
 
