61430c93415f20079e6b36f3e59943d0f94e608b
nataliaortizvi <natiorvi00@gmail.com>
2020-12-09 17:31:29 -0500

    adelanto solicitud

--- app/src/main/java/com/example/proyecto2android/DocPerroSisbenUno.java
+++ app/src/main/java/com/example/proyecto2android/DocPerroSisbenUno.java
@@ -3,8 +3,18 @@ package com.example.proyecto2android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DocPerroSisbenUno extends AppCompatActivity {
+
+    private EditText txestado;
+    private Button uploadbt;
+    private FirebaseDatabase db;
+    private FirebaseAuth auth;
 
     //aqui se pondrá la info que se debe llenar para hacer el proceso de adopción, este es eldocumento para el perro 1 de la fundación sisben para perros
 
@@ -12,5 +22,10 @@ public class DocPerroSisbenUno extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc_perro_sisben_uno);
+
+        txestado  =  findViewById(R.id.txestado);
+        uploadbt  =  findViewById(R.id.uploadbt);
+
+        db = FirebaseDatabase.getInstance();
     }
 }
