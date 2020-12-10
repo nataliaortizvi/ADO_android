07ef1dd4425a7bdbedb3ebc8c553012f55615576
nataliaortizvi <natiorvi00@gmail.com>
2020-12-10 12:56:41 -0500

    barra de navegacion

--- app/src/main/java/com/example/proyecto2android/home.java
+++ app/src/main/java/com/example/proyecto2android/home.java
@@ -1,22 +1,35 @@
 package com.example.proyecto2android;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class home extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView btnSisben, btnVet, btnAmor,btnMundo;
-    private Button home, perfil, notif;
+    private Button home, perfil, notif, salirbt;
+    private TextView nombret, correot, contrat, titulohome, tituloperfil, titulonoti;
+
+    private ConstraintLayout homeC, profileC, notiC;
+    private String usuarioIn;
+    private Usuario userlog;
 
     private FirebaseAuth auth;
     private FirebaseDatabase db;
@@ -36,22 +49,68 @@ public class home extends AppCompatActivity implements View.OnClickListener {
             home = findViewById(R.id.home);
             perfil = findViewById(R.id.perfil);
             notif = findViewById(R.id.notif);
-            btnSisben = findViewById(R.id.btnSisben);
-            btnVet = findViewById(R.id.btnVet);
-            btnAmor = findViewById(R.id.btnAmor);
-            btnMundo = findViewById(R.id.btnMundo);
+
+            homeC = findViewById(R.id.homeC);
+            profileC = findViewById(R.id.profileC);
 
             //barra de navegacion
             home.setOnClickListener(this);
             perfil.setOnClickListener(this);
             notif.setOnClickListener(this);
 
+            //elementos del home
+            btnSisben = findViewById(R.id.btnSisben);
+            btnVet = findViewById(R.id.btnVet);
+            btnAmor = findViewById(R.id.btnAmor);
+            btnMundo = findViewById(R.id.btnMundo);
+            titulohome = findViewById(R.id.titulohome);
+
             btnSisben.setOnClickListener(this);
             btnVet.setOnClickListener(this);
             btnAmor.setOnClickListener(this);
             btnMundo.setOnClickListener(this);
+
+            //elementos del perfil
+            nombret = findViewById(R.id.nombret);
+            correot = findViewById(R.id.correot);
+            contrat = findViewById(R.id.contrat);
+            salirbt = findViewById(R.id.salirbt);
+            tituloperfil = findViewById(R.id.tituloperfil);
+
+            salirbt.setOnClickListener(this);
+
+            //elementos del notificaciones
+            titulonoti = findViewById(R.id.titulonoti);
+            notiC = findViewById(R.id.notiC);
+
+            recoverUser();
         }
     }
+
+    private void recoverUser() {
+        if(auth.getCurrentUser() != null){
+            String id = auth.getCurrentUser().getUid();
+            usuarioIn = id;
+            db.getReference().child("usuarios").child(id).addListenerForSingleValueEvent( //ONCE
+                    new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot data) {
+                            userlog = data.getValue(Usuario.class);
+                            nombret.setText(userlog.getNombre());
+                            correot.setText(userlog.getCorreo());
+                            contrat.setText(userlog.getContra());
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+
+                        }
+                    }
+            );
+        }
+    }
+
+
 
     private void gotoLogin() {
         Intent i = new Intent(this, login.class);
@@ -68,19 +127,76 @@ public class home extends AppCompatActivity implements View.OnClickListener {
                 finish();
 
                 break;
-            case R.id.perfil:
-
-                break;
-            case R.id.notif:
-                break;
 
             case R.id.btnVet:
+                Intent a = new Intent(this,escoger.class);
+                startActivity(a);
+                finish();
                 break;
 
             case R.id.btnAmor:
+                Log.d(">>>>","oprimiendo pppppp");
+                Toast.makeText(this, "Esta fundación aún no tiene mascotas...", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.btnMundo:
+                Toast.makeText(this, "Esta fundación aún no tiene mascotas...", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.perfil:
+                Log.d(">>>>","oprimiendo pppppp");
+
+                homeC.setVisibility(View.GONE);
+                titulohome.setVisibility(View.GONE);
+
+                titulonoti.setVisibility(View.GONE);
+                notiC.setVisibility(View.GONE);
+
+                profileC.setVisibility(View.VISIBLE);
+                tituloperfil.setVisibility(View.VISIBLE);
 
                 break;
-            case R.id.btnMundo:
+
+            case R.id.notif:
+                Log.d(">>>>","oprimiendo nnnnn");
+
+                homeC.setVisibility(View.GONE);
+                titulohome.setVisibility(View.GONE);
+
+                profileC.setVisibility(View.GONE);
+                tituloperfil.setVisibility(View.GONE);
+
+                titulonoti.setVisibility(View.VISIBLE);
+                notiC.setVisibility(View.VISIBLE);
+
+                break;
+
+            case R.id.home:
+                Log.d(">>>>","oprimiendo hhhhhh");
+
+                homeC.setVisibility(View.VISIBLE);
+                titulohome.setVisibility(View.VISIBLE);
+
+                titulonoti.setVisibility(View.GONE);
+                notiC.setVisibility(View.GONE);
+
+                profileC.setVisibility(View.GONE);
+                tituloperfil.setVisibility(View.GONE);
+
+                break;
+
+            case R.id.salirbt:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                        .setTitle("Cerrar sesión")
+                        .setMessage("¿Esta seguro que desea cerrar sesión?")
+                        .setNegativeButton("NO", (dialog, id) -> {
+                            dialog.dismiss();
+                        })
+                        .setPositiveButton("SI", (dialog, id) -> {
+                            auth.signOut();
+                            gotoLogin();
+                        });
+                builder.show();
 
                 break;
         }
