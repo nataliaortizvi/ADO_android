93c940d4ea452bef52601dd4749486a9c19847be
Laura <isabelaleon8@gmail.com>
2020-12-11 12:35:10 -0500

    pantallitas gatito

--- app/src/main/java/com/example/proyecto2android/gatoInfoDos.java
+++ app/src/main/java/com/example/proyecto2android/gatoInfoDos.java
@@ -7,32 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class gatoUnoInfo extends AppCompatActivity implements View.OnClickListener{
-private Button btnAdop2;
+public class gatoInfoDos extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnAdop3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gato_uno_info);
-
-
-
-
-                btnAdop2  =  findViewById(R.id.btnAdop2);
-        btnAdop2.setOnClickListener(this);
-
+        setContentView(R.layout.activity_gato_info_dos);
+        btnAdop3 = findViewById(R.id.btnAdop3);
+        btnAdop3.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btnAdop2:
+            case R.id.btnAdop3:
 
-                Intent u = new Intent(this, DocPerroSisbenUno.class);
-                startActivity(u);
+                Intent h = new Intent(this, DocPerroSisbenUno.class);
+                startActivity(h);
                 finish();
 
                 break;
         }
+
+
+
     }
 }
